d93ce0a21bc979f429fde9515bdd42136aa4635f
AntwanSherif <antwansherif@gmail.com>
2022-06-19 12:40:40 +0200

    :lipstick: Wrap pages with layout

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
@@ -6,25 +5,12 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <header>
-        <h1> Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-          suscipit veritatis deserunt quos natus tempora ipsa aliquam?
-          Exercitationem esse ea consectetur. Fugiat ducimus aperiam, nam
-          nesciunt beatae repudiandae. Officiis, corporis.
-        </p>
-        <Link to={routes.home()}>Return home</Link>
-      </main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque suscipit
+        veritatis deserunt quos natus tempora ipsa aliquam? Exercitationem esse
+        ea consectetur. Fugiat ducimus aperiam, nam nesciunt beatae repudiandae.
+        Officiis, corporis.
+      </p>
     </>
   )
 }
